376e9fe5c4d40cdb6f00f8713f8b186fb5dc9956
Sergio <sergiocelemin1998@gmail.com>
2020-09-17 21:34:08 -0500

    create horizontal list component

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,17 +1,7 @@
 import React from "react";
-import Home from "../scenes/Home";
-import News from "../scenes/News";
-import Blog from "../scenes/Blog";
-import Preaches from "../scenes/Preaches";
 import Header from "../shared/Header";
 import TabNavigator from "./tabNavigator";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
-
-const Tab = createMaterialBottomTabNavigator();
 
 const Stack = createStackNavigator();
 

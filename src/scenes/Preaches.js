de988d57a5497124f4ba15693e008bff351ef731
Sergio <sergiocelemin1998@gmail.com>
2020-09-17 21:56:57 -0500

    include events and searchbar

--- src/scenes/Preaches.js
+++ src/scenes/Preaches.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function Preaches(props) {
+  
+  const [searchQuery, setSearchQuery] = useState("");
+  const onChangeSearch = (query) => setSearchQuery(query);
+
   return (
     <View style={styles.container}>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+
       <Text>Hola Preaches</Text>
     </View>
   );
@@ -12,7 +23,5 @@ export default function Preaches(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
